3ffee2c4120fcd57db6805d65b8df9ce80b5344b
Urja Rannikko <urjaman@gmail.com>
2017-06-14 22:20:09 +0300

    boop the uart to boopy speed

--- uart.h
+++ uart.h
@@ -7,7 +7,7 @@ void uart_isr(void);
 uint8_t uart_recv(void);
 void uart_send(uint8_t val);
 
-#define BAUD 115200
+#define BAUD 4000000
 
 #define RECEIVE() uart_recv()
 #define SEND(n) uart_send(n)
