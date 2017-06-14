6e2da5bf32db1771d9d61ff960884aad7c771021
Urja Rannikko <urjaman@gmail.com>
2017-06-14 13:33:44 +0300

    well, less asm-y delay :)

--- delay.c
+++ delay.c
@@ -1,34 +1,16 @@
 #include "main.h"
 #include "delay.h"
 
-
-/* This can be used from weird clocking situations, with appropriate math in caller. */
-/*
-void cycle_delay(const uint32_t cycles)
-{
-	uint32_t tmp1, tmp2;
-	asm volatile (
-		"rdcycle %[tmp1]\n\t"
-	        "1: \n\t"
-		"rdcycle %[tmp2]\n\t"
-		"sub %[tmp2], %[tmp2], %[tmp1]\n\t"
-	        "bltu %[tmp2], %[cyc], 1b\n\t"
-	        : [tmp1] "=&r" (tmp1), [tmp2] "=&r" (tmp2) : [cyc] "r" (cycles)
-	);
-}
-*/
+#include "platform.h"
+#include "encoding.h"
 
 void cycle_delay(const uint32_t cycles)
 {
-	uint32_t loops;
-	if (cycles < 2) return; /* Safety */
-	loops = cycles >> 1; // 2 cycles/loop assumption.
-	asm volatile (
-	        "1: \n\t"
-		"addi %[cyc], %[cyc], -1\n\t"
-	        "bnez %[cyc], 1b\n\t"
-	        : : [cyc] "r" (loops)
-	);
+	uint32_t start = read_csr(mcycle);
+	uint32_t passed;
+	do {
+		passed = read_csr(mcycle) - start;
+	} while (passed < cycles);
 }
 
 
