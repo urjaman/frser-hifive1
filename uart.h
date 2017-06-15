1d4a798be3b39e8093d03a6f20d0a5414fd51471
Urja Rannikko <urjaman@gmail.com>
2017-06-15 19:49:35 +0300

    F_CPU to 192Mhz so we can get good uart divider for 12Mbps
    and a commented-out 12Mbps in uart.h :)

--- uart.h
+++ uart.h
@@ -8,6 +8,8 @@ uint8_t uart_recv(void);
 void uart_send(uint8_t val);
 
 #define BAUD 4000000
+/* If your flashrom has custom baudrate support, you can try this */
+//#define BAUD 12000000
 
 #define RECEIVE() uart_recv()
 #define SEND(n) uart_send(n)
